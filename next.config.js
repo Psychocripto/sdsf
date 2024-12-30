181ff67aa135afdea444287fea6b06ab0af98ad1
Psychocripto <83782131+Psychocripto@users.noreply.github.com>
2024-12-30 23:39:30 +0530

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  experimental: {
+    appDir: true, // Enable the App Router
+  },
+};
---
+++ src/app/team/2022/page.js
@@ -0,0 +1,8 @@
+export default function Team2022() {
+    return (
+        <div>
+            <h1>Team 2022</h1>
+            <p>This is the page for the 2022 team. Add your content here.</p>
+        </div>
+    );
+}
